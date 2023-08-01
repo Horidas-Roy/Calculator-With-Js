910a3574324a3a50d310b0a511e61b4200259f09
Horidas Roy <royhoridas64@gmail.com>
2023-08-01 17:58:59 +0600

    calculator done with responsive

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-let input=document.getElementById("inputBox")
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+let string = ""; //empty string for storing result
+
+let arr = Array.from(buttons);
+
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);   //eval is a build in function
+      input.value = string;
+    }
+    else if(e.target.innerHTML == "AC"){
+              string=""
+              input.value=string;
+    }
+    else if(e.target.innerHTML == "DEL"){
+        string=string.substring(0, string.length-1);
+        input.value=string;
+    }
+     else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
